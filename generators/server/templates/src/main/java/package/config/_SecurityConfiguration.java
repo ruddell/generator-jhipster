bdc7ae8edad75dc3da2b9fb1c9ed540363744c61
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2017-09-17 15:40:03 -0700

    put CorsFilter before CsrfFilter for session apps

--- generators/server/templates/src/main/java/package/config/_SecurityConfiguration.java
+++ generators/server/templates/src/main/java/package/config/_SecurityConfiguration.java
@@ -50,10 +50,13 @@ import org.springframework.security.data.repository.query.SecurityEvaluationCont
 <%_ if (authenticationType === 'session') { _%>
 import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfFilter;
 <%_ } _%>
 <%_ if (authenticationType !== 'oauth2') { _%>
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
+<%_ } _%>
+<%_ if (authenticationType === 'jwt') { _%>
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 <%_ } _%>
 
 import javax.annotation.PostConstruct;
@@ -179,7 +182,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
             <%_ } _%>
             <%_ if (authenticationType !== 'oauth2') { _%>
-            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(corsFilter, <% if (authenticationType === 'jwt') { %>UsernamePasswordAuthenticationFilter.class<% } else if (authenticationType === 'session') { %>CsrfFilter.class<% } %>)
             <%_ } _%>
             .exceptionHandling()
             .authenticationEntryPoint(http401UnauthorizedEntryPoint())<% if (authenticationType === 'session') { %>
